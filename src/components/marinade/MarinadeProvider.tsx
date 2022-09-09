605430125f8306e1f853ff912faad3bee0da48c8
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-09 14:14:34 +0200

    changes for local development

--- src/components/marinade/MarinadeProvider.tsx
+++ src/components/marinade/MarinadeProvider.tsx
@@ -27,9 +27,20 @@ export const MarinadeProvider: FC<MarinadeProviderProps> = ({ children }) => {
     }
 
     // !!! You have to put in your own Referral Code on the line below !!!
-    const referralCode = new web3.PublicKey('mRtnRH2M3rMLP4BBcrxkk4WBKsSi3JvoyUEog7gf3qE')
+    const referralCode = new web3.PublicKey('CRiqV9QCSJ2GLrMDSE8yi9MzSzMvNY2fActLQXdxz2fv')  // partner referral account / referral-keypair from setup-referral
     // !!! Then uncomment the `referralCode` configuration option !!!
-    const config = new MarinadeConfig({ connection, publicKey/*, referralCode*/ })
+
+    const localConnection = new web3.Connection("http://localhost:8899")
+    const config = new MarinadeConfig({
+      connection: localConnection,
+      publicKey,  // wallet pubkey
+      marinadeFinanceProgramId: new web3.PublicKey('MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD'),  // this is default value
+      marinadeReferralProgramId: new web3.PublicKey('NTxC2LBrYLBtCmzTUJCWTxiHMYCJfeiEgzkhxCaGM3S'),
+      marinadeStateAddress: new web3.PublicKey('6d5c9xHMWWmwd8eFfvqhSnXdHMSYcJzwyPmXEpykqSxp'),  // marinade instance
+      marinadeReferralGlobalStateAddress: new web3.PublicKey('AR9emZP2faAG9NAM5nKvPt2j9w4piU2d5VpeA8TGy7vA'),
+      referralCode,   // partner referral state account
+      referralPartnerAccount: new web3.PublicKey('2f7svTzicpawmpywwfnGfv5T8UxP2sb4yrTkePF4GQWL'), // partner beneficiary account (partner main account)
+    })
     const marinade = new Marinade(config)
 
     setMarinade(marinade)
